c0b8a5d25ce961f80a2ddfa769e470ba5d315cfb
Stuart Harlan <stuart.harlan@outlook.com>
2022-02-26 19:34:35 -0600

    add more tests with partial packets and random bytes

--- rt/Program.cs
+++ rt/Program.cs
@@ -23,6 +23,23 @@ var SocketSend = async (String p, String expectedStatusCode) =>
     s.Dispose();
 };
 
+var SocketSendBytes = async (byte[] pb, String expectedStatusCode) =>
+{
+    Console.WriteLine("========================================");
+    Console.WriteLine($"Testing packet of bytes");
+    ArraySegment<byte> buffer = new ArraySegment<byte>(pb);
+    Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    await s.ConnectAsync(new IPEndPoint(IPAddress.Loopback, 27015));
+    await s.SendAsync(buffer, SocketFlags.None);
+    byte[] rcvBuffer = new byte[8196];
+    ArraySegment<byte> rcvBufferSeg = new ArraySegment<byte>(rcvBuffer);
+    int bytesRcvd = await s.ReceiveAsync(rcvBufferSeg, SocketFlags.None);
+    String respString = System.Text.Encoding.UTF8.GetString(rcvBuffer);
+    Assert.True(respString.StartsWith($"HTTP/1.1 {expectedStatusCode}"));
+    s.Close();
+    s.Dispose();
+};
+
 HttpClient client = new HttpClient();
 
 Console.WriteLine("========================================");
@@ -93,3 +110,22 @@ await SocketSend("GET index.html", "400");
 await SocketSend("GET index.html\r\n", "400");
 await SocketSend("GET index.html HTTP/1.1", "400");
 await SocketSend("GET /../../blah HTTP/1.1\r\nHost:localhost\r\n\r\n", "403");
+
+String base_packet = "GET index.html HTTP/1.1\r\nHost:localhost\r\n\r\n";
+for(int i=0; i<base_packet.Length + 1; i++)
+{
+    byte[] extraBytes = new byte[1024];
+    var rand = new Random();
+    rand.NextBytes(extraBytes);
+    String sub_packet = base_packet.Substring(0, i);
+    byte[] sub_packet_bytes = System.Text.Encoding.UTF8.GetBytes(sub_packet);
+    Array.Copy(sub_packet_bytes, extraBytes, sub_packet_bytes.Length);
+    if(i == base_packet.Length)
+    {
+        await SocketSendBytes(extraBytes, "200");
+    }
+    else
+    {
+        await SocketSendBytes(extraBytes, "400");
+    }
+}
